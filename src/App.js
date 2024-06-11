bae6f40a520f6b5c4d7e943120134f7f6d20422d
soheil <razouq.soheil@gmail.com>
2024-06-11 12:04:17 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './App.scss';
+import Header from './components/Header.js';
+import HomeBanner from "./components/HomeBanner";
+import Login from "./components/Login";
+import Banner from "./components/Banner";
+import List from "./components/List";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Router>
+        <Routes>
+          <Route path="/" element={
+            <React.Fragment>
+              <Header/>
+              <HomeBanner/>
+            </React.Fragment>
+          }/>
+          <Route path="/login" element={
+            <React.Fragment>
+              <Header/>
+              <Login/>
+            </React.Fragment>
+          }/>
+          <Route path="/register" element={
+            <React.Fragment>
+              <Header/>
+              <Login/>
+            </React.Fragment>
+          }/>
+          <Route path="/dashboard" element={
+            <React.Fragment>
+              <Header/>
+              <Banner/>
+              <List title="Netflix Originals" param="originals"/>
+              <List title="Trending Now" param="trending"/>
+              <List title="Now Playing" param="now_playing"/>
+              <List title="popular" param="popular"/>
+              <List title="Top Rated" param="top_rated"/>
+              <List title="Upcoming" param="upcoming"/>
+            </React.Fragment>
+          }/>
+        </Routes>
+      </Router>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBaNxJ2q8v6iHnE4uGL6MdH78RpSaJqgro",
+  authDomain: "movie-app-74d78.firebaseapp.com",
+  projectId: "movie-app-74d78",
+  storageBucket: "movie-app-74d78.appspot.com",
+  messagingSenderId: "400835767984",
+  appId: "1:400835767984:web:63ea62025689f05ac7d575",
+  measurementId: "G-W7WNNFG02E"
+};
